ea3b73ccdd02bab95d575e6ee18f2aebb358aa18
wellchikhack <fortgshw6@gmail.com>
2026-03-17 22:17:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ from aiogram.filters import CommandStart
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
+from aiohttp import web
+
+# Простейший хендлер
+async def handle(request):
+    return web.Response(text="Bot is running")
+
+# Функция для запуска веб-сервера параллельно с ботом
+async def on_startup(dispatcher):
+    app = web.Application()
+    app.router.add_get("/", handle)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", int(os.getenv("PORT", 8080)))
+    await site.start()
 
 TOKEN = os.getenv ("TOKEN")
 ADMIN_ID = 8778662008
